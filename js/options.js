d6aa353a4e89b48eca93eb13ed90b09f578d9efc
Yoshiaki Murakami <info@ysakmrkm.com>
2013-02-17 16:38:29 +0900

    separete files

---
+++ js/options.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    tumblrTile.loadConfig();
+
+    var $baseWidth = $('#setting input[name="baseWidth"]');
+    var $margin    = $('#setting input[name="margin"]');
+
+    $baseWidth.val(tumblrTile.config.baseWidth);
+    $margin.val(tumblrTile.config.margin);
+
+    $("#setting").submit(function() {
+        var hash = {
+            baseWidth: parseInt($baseWidth.val()),
+            margin   : parseInt($margin.val())
+        };
+
+        tumblrTile.saveConfig(hash);
+        return false;
+    });
+});
